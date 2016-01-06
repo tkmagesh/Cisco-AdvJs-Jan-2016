87dc99c66097ad626c4005c969b856056c4c9597
tkmagesh <tkmagesh77@gmail.com>
2016-01-06 09:46:24 +0530

    memoization

--- Day-01/03-Closures/isPrime.js
+++ Day-01/03-Closures/isPrime.js
@@ -9,7 +9,7 @@ isPrime(102) => do not run the algo
 isPrime(103) => do not run the algo
 
 var isPrime = (function(){
-    var cache = {};
+
     function checkPrime(n){
         console.log("processing ", n);
         if (n <= 3) return true;
@@ -17,9 +17,38 @@ var isPrime = (function(){
             if (n % i === 0) return false;
         return true;
     }
+
+    var cache = {};
     return function (n){
         if (typeof cache[n] === 'undefined')
             cache[n] = checkPrime(n);
         return cache[n];
     }
 })();
+
+
+var cachedAdd = (function(){
+
+    function add(x,y){
+        console.log("processing ", x , " and ", y);
+        return x + y;
+    }
+
+    var cache = {};
+    return function(x,y){
+        var key = JSON.stringify(arguments);
+        if (typeof cache[key] === 'undefined')
+            cache[key] = add(x,y);
+        return cache[key];
+    }
+})();
+
+function memoize(fn){
+    var cache = {};
+    return function(){
+        var key = JSON.stringify(arguments);
+        if (typeof cache[key] === 'undefined')
+            cache[key] = fn.apply(this,arguments);
+        return cache[key];
+    }
+}
